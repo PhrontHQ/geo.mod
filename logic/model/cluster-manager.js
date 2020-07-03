c1c6411bb5e89e7247ae302b451d10c056473678
Benoit Marchant <marchant@mac.com>
2020-07-02 18:40:47 -0700

    fix montage collections require

--- logic/model/cluster-manager.js
+++ logic/model/cluster-manager.js
@@ -1,6 +1,6 @@
 var Montage = require("montage").Montage,
-    Map = require("montage/collections/map"),
-    Set = require("montage/collections/set");
+    Map = require("montage/core/collections/map"),
+    Set = require("montage/core/collections/set");
 
 /**
  * @class ClusterManager
